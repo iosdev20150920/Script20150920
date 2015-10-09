8d7bddd43b263bfe006e66fa78a706a76a9e3b2a
iosdev20150920 <iosdev20150920@users.noreply.github.com>
2015-10-09 20:39:36 +0800

    【Python 0020 2015-10-09】用Python编写IOS工程的自动编译脚本
    搭建脚本框架，通过简单代码的测试
    check out 工程代码

--- Python20150920/Python20150920/Demo20151009/svnCheckout/svnCheckout.py
+++ Python20150920/Python20150920/Demo20151009/svnCheckout/svnCheckout.py
@@ -5,7 +5,46 @@ yangfs
 2015-10-09
 '''
 #!/usr/bin/python
+import os
 
-def svnCheckout(self):
-	print("svn Checkout source start:")
-	# os.systemt
+class SourceSvnCheckout:
+	"check out 工程代码 类"
+
+	def __init__(self, svnPath="", svnUserName="", svnPassword="", codeDir=""):
+		self.svnPath = svnPath
+		self.svnUserName= svnUserName
+		self.svnPassword = svnPassword
+		self.codeDir = codeDir;
+
+		self.buildSucced = False
+		self.buildErrorDescription = []
+		
+	def checkoutCode(self):
+		"下载代码的方法"
+
+		print("svn Checkout source start:")
+
+		codeDir = self.codeDir
+		svnInfo = "%s --username %s -- password %s"%(self.svnPath, self.svnUserName, self.svnPassword)
+		os.system('rm -frd %s '%codeDir);
+		os.system('mkdir %s'%codeDir);
+
+		svnCmd = 'svn checkout %s -- no-interactive %s'%(svnInfo, codeDir)
+		os.system(svnCmd);
+		print("svn语句：%s"%(svnCmd))
+
+		retValue = False;
+		msg = 'checkOut code failed'
+		if os.path.exists('%sYourProjectName'%codeDir) == False:
+			msg = 'checkOut code failed'
+			print(msg)
+			self.buildSucced = False
+			self.buildErrorDescription.append(msg)
+		else:
+			msg = 'checkOut code success!'
+			# os.chdir(LifeSearchBuild.codeDir)
+			retValue = True
+
+		return msg
+
+
--- Python20150920/Python20150920/Demo20151009/test.py
+++ Python20150920/Python20150920/Demo20151009/test.py
@@ -11,6 +11,12 @@ from svnCheckout.svnCheckout import *
 # sys.path.insert(0, "/Library/Python/2.7/site-packages");
 
 print("check out 工程代码：")
-v = svnCheckout(0);
+
+svnPath = "https://github.com/iphoneReadingme/SpriteKitCode20150830.git/trunk/03-Particles/step01"
+svnUserName= "iosdev20150920@126.com"
+svnPassword = "yang346854"
+codeDir = "/Users/yangfs/Desktop/E/Src20150915/Src20151009/SvnDemo20151009/";
+svnObject = SourceSvnCheckout(svnPath, svnUserName, svnPassword, codeDir)
+v = svnObject.checkoutCode()
 
 print(v);
